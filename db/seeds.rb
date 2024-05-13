1d2fbee339655d4a0004934772779d71645cf4c6
Louise Ouldhaddi <ouldhaddi@gmail.com>
2024-05-13 14:21:29 +0200

    creating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+5.times do
+  Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, category: %w(chinese italian japanese french belgian).sample)
+end
+
+puts "Finished!"
